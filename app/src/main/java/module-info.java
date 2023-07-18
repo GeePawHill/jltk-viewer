1906a344526d1411cb89a2f11fbf56218a7ebc52
GeePaw Hill <geepawhill@geepawhill.org>
2023-07-17 23:15:17 -0400

    Revert "And again."
    This reverts commit d75c3c56cc0936612bc906712303734e2f590852.

--- app/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module dev.vulture.packagefx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports dev.vulture.packagefx;
-    opens dev.vulture.packagefx.controller to javafx.fxml;
-}
