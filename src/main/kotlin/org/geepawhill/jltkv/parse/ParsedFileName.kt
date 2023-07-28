4be81e236081ca8e3ec699ace1d702eebca50e30
GeePaw Hill <geepawhill@geepawhill.org>
2023-07-28 10:37:26 -0400

    Tightened down ParsedFileName.

--- src/main/kotlin/org/geepawhill/jltkv/parse/ParsedFileName.kt
+++ src/main/kotlin/org/geepawhill/jltkv/parse/ParsedFileName.kt
@@ -19,33 +19,32 @@ class ParsedFileName(val name: String) {
             branch = NA
             email = NA
             filestamp = EARLIEST_FILESTAMP
-            timestamp = timestampFrom(filestamp)
         } else {
             branch = result.groups[1]!!.value
             email = result.groups[2]!!.value
             filestamp = result.groups[3]!!.value
-            timestamp = timestampFrom(filestamp)
         }
+        timestamp = timestampFrom(filestamp)
     }
 
     companion object {
         const val NA = "N/A"
         const val EARLIEST_FILESTAMP = "20200101000000"
-        val EARLIEST_TIMESTAMP = timestampFrom(LocalDateTime.of(2020, 1, 1, 0, 0, 0))
+        val EARLIEST_TIMESTAMP = timestampFrom(EARLIEST_FILESTAMP)
 
         fun timestampFrom(filestamp: String): String {
             try {
                 val time = filetimeFormatter.parse(filestamp, LocalDateTime::from)
-                return timestampFrom(time)
+                return dropFractionalSeconds(time)
             } catch (ignored: Exception) {
                 return EARLIEST_TIMESTAMP
             }
         }
 
-        fun timestampFrom(time: LocalDateTime): String {
+        private fun dropFractionalSeconds(time: LocalDateTime): String {
             return time.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME).split("\\.")[0]
         }
 
-        var filetimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddkkmmss")
+        private var filetimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddkkmmss")
     }
 }
